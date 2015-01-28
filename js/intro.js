cbdf2b80a3e68f968bc9a42caffe1dac79172f36
JingFei <jingfei955047@gmail.com>
2015-01-28 17:31:13 +0800

    finish jquery circular progress!!

--- js/intro.js
+++ js/intro.js
@@ -5,26 +5,183 @@ $(document).ready( function() {
 	$("#iT1").hover(function(){ShowIntro(1);});
 	$("#iT2").hover(function(){ShowIntro(2);});
 	$("#iT3").hover(function(){ShowIntro(3);});
+		$('.first').circleProgress({
+			value: 0.75,
+			size: 150.0,
+			startAngle: -Math.PI/2,
+			thickness: 8,
+			fill: { color: '#FFEE00' },
+		}).on('circle-animation-progress', function(event, progress) {
+			$(this).find('.circleText').html(parseInt(75 * progress) + '<i>%</i>');
+		});
+		$('.second').circleProgress({
+			value: 0.8,
+			size: 150.0,
+			startAngle: -Math.PI/2,
+			thickness: 8,
+			fill: { color: '#FFEE00' },
+		}).on('circle-animation-progress', function(event, progress) {
+			$(this).find('.circleText').html(parseInt(80 * progress) + '<i>%</i>');
+		});
+		$('.third').circleProgress({
+			value: 0.65,
+			size: 150.0,
+			startAngle: -Math.PI/2,
+			thickness: 8,
+			fill: { color: '#FFEE00' },
+		}).on('circle-animation-progress', function(event, progress) {
+			$(this).find('.circleText').html(parseInt(65 * progress) + '<i>%</i>');
+		});
+		$('.fourth').circleProgress({
+			value: 0.85,
+			size: 150.0,
+			startAngle: -Math.PI/2,
+			thickness: 8,
+			fill: { color: '#FFEE00' },
+		}).on('circle-animation-progress', function(event, progress) {
+			$(this).find('.circleText').html(parseInt(85 * progress) + '<i>%</i>');
+		});
 });
 
 function ShowIntro(id){
+	event.stopPropagation();
 	if(id===1){
 		$("#tr02").animate({"left":"-295px"},300);
 		$("#intro1").show("slow");
 		$("#intro2").hide();
 		$("#intro3").hide();
+		$("#cT1").html("Brainstorming ideas");
+		$("#cT2").html("Brand Packaging");
+		$("#cT3").html("Front-End Development");
+		$("#cT4").html("UI/UX Design");
+		$('.first').circleProgress({
+			value: 0.75,
+			size: 150.0,
+			startAngle: -Math.PI/2,
+			thickness: 8,
+			fill: { color: '#FFEE00' },
+		}).on('circle-animation-progress', function(event, progress) {
+			$(this).find('.circleText').html(parseInt(75 * progress) + '<i>%</i>');
+		});
+		$('.second').circleProgress({
+			value: 0.8,
+			size: 150.0,
+			startAngle: -Math.PI/2,
+			thickness: 8,
+			fill: { color: '#FFEE00' },
+		}).on('circle-animation-progress', function(event, progress) {
+			$(this).find('.circleText').html(parseInt(80 * progress) + '<i>%</i>');
+		});
+		$('.third').circleProgress({
+			value: 0.65,
+			size: 150.0,
+			startAngle: -Math.PI/2,
+			thickness: 8,
+			fill: { color: '#FFEE00' },
+		}).on('circle-animation-progress', function(event, progress) {
+			$(this).find('.circleText').html(parseInt(65 * progress) + '<i>%</i>');
+		});
+		$('.fourth').circleProgress({
+			value: 0.85,
+			size: 150.0,
+			startAngle: -Math.PI/2,
+			thickness: 8,
+			fill: { color: '#FFEE00' },
+		}).on('circle-animation-progress', function(event, progress) {
+			$(this).find('.circleText').html(parseInt(85 * progress) + '<i>%</i>');
+		});
 	}
 	else if(id===2){
 		$("#tr02").animate({"left":"0"},300);
 		$("#intro2").slideDown("slow");
 		$("#intro1").hide();
 		$("#intro3").hide();
+		$("#cT1").html("企");
+		$("#cT2").html("劃");
+		$("#cT3").html("組");
+		$("#cT4").html("啦");
+		$('.first').circleProgress({
+			value: 0.77,
+			size: 150.0,
+			startAngle: -Math.PI/2,
+			thickness: 8,
+			fill: { color: '#FFEE00' },
+		}).on('circle-animation-progress', function(event, progress) {
+			$(this).find('.circleText').html(parseInt(77 * progress) + '<i>%</i>');
+		});
+		$('.second').circleProgress({
+			value: 0.99,
+			size: 150.0,
+			startAngle: -Math.PI/2,
+			thickness: 8,
+			fill: { color: '#FFEE00' },
+		}).on('circle-animation-progress', function(event, progress) {
+			$(this).find('.circleText').html(parseInt(99 * progress) + '<i>%</i>');
+		});
+		$('.third').circleProgress({
+			value: 0.88,
+			size: 150.0,
+			startAngle: -Math.PI/2,
+			thickness: 8,
+			fill: { color: '#FFEE00' },
+		}).on('circle-animation-progress', function(event, progress) {
+			$(this).find('.circleText').html(parseInt(88 * progress) + '<i>%</i>');
+		});
+		$('.fourth').circleProgress({
+			value: 0.75,
+			size: 150.0,
+			startAngle: -Math.PI/2,
+			thickness: 8,
+			fill: { color: '#FFEE00' },
+		}).on('circle-animation-progress', function(event, progress) {
+			$(this).find('.circleText').html(parseInt(75 * progress) + '<i>%</i>');
+		});
 	}
 	else if(id===3){
 		$("#tr02").animate({"left":"295px"},300);
 		$("#intro3").show("slide",{direction:"right"},"slow");
 		$("#intro2").hide();
 		$("#intro1").hide();
+		$("#cT1").html("技");
+		$("#cT2").html("術");
+		$("#cT3").html("組");
+		$("#cT4").html("啊");
+		$('.first').circleProgress({
+			value: 1,
+			size: 150.0,
+			startAngle: -Math.PI/2,
+			thickness: 8,
+			fill: { color: '#FFEE00' },
+		}).on('circle-animation-progress', function(event, progress) {
+			$(this).find('.circleText').html(parseInt(100 * progress) + '<i>%</i>');
+		});
+		$('.second').circleProgress({
+			value: 0.7,
+			size: 150.0,
+			startAngle: -Math.PI/2,
+			thickness: 8,
+			fill: { color: '#FFEE00' },
+		}).on('circle-animation-progress', function(event, progress) {
+			$(this).find('.circleText').html(parseInt(70 * progress) + '<i>%</i>');
+		});
+		$('.third').circleProgress({
+			value: 0.9,
+			size: 150.0,
+			startAngle: -Math.PI/2,
+			thickness: 8,
+			fill: { color: '#FFEE00' },
+		}).on('circle-animation-progress', function(event, progress) {
+			$(this).find('.circleText').html(parseInt(90 * progress) + '<i>%</i>');
+		});
+		$('.fourth').circleProgress({
+			value: 0.95,
+			size: 150.0,
+			startAngle: -Math.PI/2,
+			thickness: 8,
+			fill: { color: '#FFEE00' },
+		}).on('circle-animation-progress', function(event, progress) {
+			$(this).find('.circleText').html(parseInt(95 * progress) + '<i>%</i>');
+		});
 	}
 }
 
