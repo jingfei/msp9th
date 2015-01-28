dba837e95c146fed535ca00f9416d48ee3af56b6
JingFei <jingfei955047@gmail.com>
2015-01-28 13:37:05 +0800

    wrong typing @@

--- js/home.js
+++ js/home.js
@@ -1,56 +1,4 @@
 $(document).ready( function() {
-//	$("img.circle").hover(
-//	  function(){
-//		$(this).attr("src", "images/circle-gray.png");
-//	},function(){
-//		if($(this).attr("class").search("ActiveCircle")==-1)
-//			$(this).attr("src", "images/circle-black.png");
-//	});
-//	$("#c1").click(function(){
-//		$(".gates").hide("slow",function(){
-//			$("#p1").show();
-//			$("#c1").attr("src", "images/circle-gray.png");
-//		});
-//		$("img.circle").each(function(){
-//			if( $(this).attr("id")==="c1" ){
-//				$(this).addClass("ActiveCircle");
-//			}
-//			else{
-//				$(this).attr("src","images/circle-black.png");
-//				$(this).attr("class","circle");
-//			}
-//		});
-//	});
-//	$("#c2").click(function(){
-//		$(".gates").hide("slow",function(){
-//			$("#p2").show();
-//			$("#c2").attr("src", "images/circle-gray.png");
-//		});
-//		$("img.circle").each(function(){
-//			if( $(this).attr("id")==="c2" ){
-//				$(this).addClass("ActiveCircle");
-//			}
-//			else{
-//				$(this).attr("src","images/circle-black.png");
-//				$(this).attr("class","circle");
-//			}
-//		});
-//	});
-//	$("#c3").click(function(){
-//		$(".gates").hide("slow",function(){
-//			$("#p3").show();
-//			$("#c3").attr("src", "images/circle-gray.png");
-//		});
-//		$("img.circle").each(function(){
-//			if( $(this).attr("id")==="c3" ){
-//				$(this).addClass("ActiveCircle");
-//			}
-//			else{
-//				$(this).attr("src","images/circle-black.png");
-//				$(this).attr("class","circle");
-//			}
-//		});
-//	});
 	var myCircle=setInterval(ChangeCircle, 3000);
 });
 
@@ -96,7 +44,7 @@ function ChangeCircle(){
 				$(this).addClass("ActiveCircle");
 			}
 			else{
-				$(this).attr("src","img/circle-darblue.png");
+				$(this).attr("src","img/circle-darkblue.png");
 				$(this).attr("class","circle");
 			}
 		});
