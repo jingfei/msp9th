a2692bf7fc0d7ad5d920614364c02efa1cac04f6
JingFei <jingfei955047@gmail.com>
2015-02-05 15:41:59 +0800

    fix group effect bug

--- js/group.js
+++ js/group.js
@@ -34,60 +34,49 @@ $(document).ready( function() {
 });
 
 function ShowGroup(id){
-	var i=0;
-	var member=$("#member").find('.person');
 	if(id===1){
 		$("#gI1").attr("src","img/group_10.png");
 		$("#gI2").attr("src","img/group_21.png");
 		$("#gI3").attr("src","img/group_31.png");
-		member.each(function(){
+		$("#member").find(".person").each(function(){
 			$(this).delay(50).hide(400);
-			++i;
-			if(i==member.length){
-				$("#gM2").delay(400).hide();
-				$("#gM3").delay(400).hide();
-				$("#gM1").delay(400).show();
-				$("#gM1").find('.person').each(function(){
-					$(this).show("slow");
-					$(this).css("display","inline-block");
-				});
-			}
+		});
+		$("#gM2").delay(450).hide(0);
+		$("#gM3").delay(450).hide(0);
+		$("#gM1").delay(400).show(0);
+		$("#gM1").delay(450).find('.person').each(function(){
+			$(this).show("slow");
+			$(this).css("display","inline-block");
 		});
 	}
 	else if(id==2){
 		$("#gI1").attr("src","img/group_11.png");
 		$("#gI2").attr("src","img/group_20.png");
 		$("#gI3").attr("src","img/group_31.png");
-		member.each(function(){
-			$(this).delay(50).slideUp();
-			++i;
-			if(i==member.length){
-				$("#gM1").delay(400).hide();
-				$("#gM3").delay(400).hide();
-				$("#gM2").delay(400).show();
-				$("#gM2").find('.person').each(function(){
-					$(this).slideDown("slow");
-					$(this).css("display","inline-block");
-				});
-			}
+		$("#member").find(".person").each(function(){
+			$(this).delay(50).fadeOut(400);
+		});
+		$("#gM1").delay(450).hide(0);
+		$("#gM3").delay(450).hide(0);
+		$("#gM2").delay(400).show(0);
+		$("#gM2").delay(450).find('.person').each(function(){
+			$(this).fadeIn("slow");
+			$(this).css("display","inline-block");
 		});
 	}
 	else if(id==3){
 		$("#gI1").attr("src","img/group_11.png");
 		$("#gI2").attr("src","img/group_21.png");
 		$("#gI3").attr("src","img/group_30.png");
-		member.each(function(){
-			$(this).delay(50).fadeOut();
-			++i;
-			if(i==member.length){
-				$("#gM1").hide();
-				$("#gM2").hide();
-				$("#gM3").show();
-				$("#gM3").find('.person').each(function(){
-					$(this).fadeIn("slow");
-					$(this).css("display","inline-block");
-				});
-			}
+		$("#member").find(".person").each(function(){
+			$(this).delay(50).slideUp(400);
+		});
+		$("#gM1").delay(450).hide(0);
+		$("#gM2").delay(450).hide(0);
+		$("#gM3").delay(400).show(0);
+		$("#gM3").delay(450).find('.person').each(function(){
+			$(this).slideDown("slow");
+			$(this).css("display","inline-block");
 		});
 	}
 }
