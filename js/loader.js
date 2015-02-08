34ac375d97155c451b796b041b155e26f6ec51b8
JingFei <jingfei955047@gmail.com>
2015-02-09 01:11:45 +0800

    finish loader!!

--- js/loader.js
+++ js/loader.js
@@ -13,7 +13,7 @@ var opts = {
 	hwaccel: false, // Whether to use hardware acceleration
 	className: 'spinner', // The CSS class to assign to the spinner
 	zIndex: 2e9, // The z-index (defaults to 2000000000)
-	top: '50%', // Top position relative to parent
+	top: '55%', // Top position relative to parent
 	left: '50%' // Left position relative to parent
 };
 var target = document.getElementById('loader');
