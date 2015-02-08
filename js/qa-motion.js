b2d660221d4067d749f0e3440b595b6cbe852217
stu1130 <stu1130@hotmail.com.tw>
2015-02-08 22:09:45 +0800

    add inovative resume

--- js/qa-motion.js
+++ js/qa-motion.js
@@ -66,4 +66,20 @@ $(function()
 		}
 		$qa_content.slideToggle();
 	}).siblings().addClass('Ans').hide();
+	
+	$('#qa_5 ul').addClass('accordionPart').find('li div:nth-child(1)').addClass('Ques').hover(function()
+	{
+		$(this).addClass('Ques_on');
+	}, function()
+	{
+		$(this).removeClass('Ques_on');
+	}).click(function()
+	{
+		var $qa_content = $(this).next('div.Ans');
+		if(!$qa_content.is(':visible'))
+		{
+			$('#qa_5 ul li div.Ans:visible').slideUp();
+		}
+		$qa_content.slideToggle();
+	}).siblings().addClass('Ans').hide();
 });
