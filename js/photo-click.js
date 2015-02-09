f3278cf5e67d2d74782426edce9d3dc2c441b959
karenchao <karenchaohaha@gmail.com>
2015-02-09 14:46:14 +0800

    update mspdetail

--- js/photo-click.js
+++ js/photo-click.js
@@ -1,21 +1,51 @@
 $(document).ready(function () {
-    $("article").click(function () {
+    $(".person > article").click(function () {
         $('body').css({'overflow' : 'hidden'});
         $(document).bind('scroll', function () {
             window.scrollTo(0, 0);
         });
-        var height = $("html").height();
-        var width = $("html").width();
-        //$("#mask table").css("background", "rgba(0%,0%,0%,0.9)");
-        $("#mask").css({"width" : width, "height" : height});
-        $("#mask").show();
         
+        //var height = $("html").height();
+        //var width = $("html").width();
+        //$("#mask").css({"width" : width, "height" : height});
+        
+        var group = $(this).siblings("#persongroup").html();
+        var school = $(this).siblings("#personschool").html();
+        var name = $(this).siblings("#personname").html();
+        var skill = $(this).siblings("#personskill").html();
+        var say = $(this).siblings("#personsay").html();
+        var img = $(this).siblings("img").attr("src");
+        $("#mspgroup").html(group);
+        $("#mspschool").html(school);
+        $("#mspname").html(name);
+        $("#mspskill").html(skill);
+        $("#mspsay").html(say);
+        $("#mspdetail #mspimage").attr("src", img);
+        
+        $("#mspdetail").fadeIn(200);
+        $("#mask").fadeIn(200);
+        $("#whitemask").fadeIn(200);
+        $("#bluemask").fadeIn(200);
     });
     
     $("#mask").click(function () {
-        $("#mask").hide();
         $(document).unbind('scroll');
         $('body').css({'overflow' : 'visible'});
+        
+        $("#mspdetail").fadeOut(200);
+        $("#mask").fadeOut(200);
+        $("#whitemask").fadeOut(200);
+        $("#bluemask").fadeOut(200);
+    });
+    
+    $("#exit").click(function () {
+        $(document).unbind('scroll');
+        $('body').css({'overflow' : 'visible'});
+        
+        $("#mspdetail").fadeOut(200);
+        $("#mask").fadeOut(200);
+        $("#whitemask").fadeOut(200);
+        $("#bluemask").fadeOut(200);
     });
 });
 
